c43b616e127edf5c6791fe81f59c2cfa461f4df4
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-04 19:23:20 -0400

    mvp commit for print.py

--- Print.py
+++ Print.py
@@ -1,13 +1,35 @@
 # What keyword tells Python to display something on the screen?
-print ()
+print()
 
 # Complete the line of code to tell Python to display the name of the oval object laid by a hen. (1 word, lower-case)
 # print________
-print ("egg") 
+print("egg") 
 
 # Tell Python to display the letter "e".
-print ("e")
+print("e")
 
 # Tell Python to display the name of our planet. (1 word, lower-case)
-print ("earth")
+print("earth")
 
+# Tell Python to display the name of the star that makes life possible on earth. (1 word, lower-case  
+print ("sun")
+
+# In a print statement, the characters surrounded by quotation marks are a text _______.
+print("hello world")
+
+# string
+
+# Tell Python to display the first three letters of the alphabet in upper-case.
+print("ABC")
+
+# Tell Python to display a string of your choice
+print ("HI ,Hello")
+
+# Tell Python to display your first name.
+print("Justin")
+
+# Tell Python to display the three-letter word for a canine. (1 word, lower-case)
+print("Dog")
+
+# write a statement telling Python to display the string "Hello, World!"
+print("Hello, World")
