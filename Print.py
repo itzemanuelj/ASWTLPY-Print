eafa46f1d1bef0b72e2bdf00bca8f4e12a0f21c0
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-04 19:10:31 -0400

    intial commit

---
+++ Print.py
@@ -0,0 +1,13 @@
+# What keyword tells Python to display something on the screen?
+print ()
+
+# Complete the line of code to tell Python to display the name of the oval object laid by a hen. (1 word, lower-case)
+# print________
+print ("egg") 
+
+# Tell Python to display the letter "e".
+print ("e")
+
+# Tell Python to display the name of our planet. (1 word, lower-case)
+print ("earth")
+
